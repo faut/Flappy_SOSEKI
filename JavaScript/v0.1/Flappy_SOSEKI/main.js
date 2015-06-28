8e810e2224585e86e4fa51aafb76275c87c36bc3
faut <pippipipi@live.jp>
2015-06-28 19:03:17 +0900

    js 0.0.0.1

--- JavaScript/v0.1/Flappy_SOSEKI/main.js
+++ JavaScript/v0.1/Flappy_SOSEKI/main.js
@@ -2,20 +2,42 @@ enchant();
 window.onload = function() {
     game = new Game(450, 800);
     game.fps = 60;
-    game.preload('img/SOSEKI_100x100.png');
+game.preload('img/back.png','img/click.png','img/exit.png','img/gameover.png','img/SOSEKI_100x100.png','img/kuma_pipe_400_750.png');
+	game.rootScene.backgroundColor = "#ffffff"; // ゲームの背景色を黒色に設定
+ 	game.score = 0;  // スコアを入れる変数を用意する
+	 // スコアを表示するラベルを作成
+	 var scoreLabel = new Label("SCORE : 0");
+	 scoreLabel.font = "16px Tahoma";
+	 scoreLabel.color = "red";
+	 scoreLabel.x = 10; // X座標
+	 scoreLabel.y = 5; // Y座標
     game.onload = function() {
-        var sprite = new Sprite(50, 25);
-        sprite.image = game.assets['img/SOSEKI_100x100.png'];
-        game.rootScene.addChild(sprite);
+        var SOSEKI = new Sprite(50, 25);
+        SOSEKI.image = game.assets['img/SOSEKI_100x100.png'];
+	        var BACK1 = new Sprite(50, 25);
+		BACK1.image = game.assets['img/back.png'];
+	        var BACK2 = new Sprite(50, 25);
+		BACK2.image = game.assets['img/back.png'];
+	        var CLICK = new Sprite(50, 25);
+		CLICK.image = game.assets['img/click.png'];
+	        var EXIT = new Sprite(50, 25);
+		EXIT.image = game.assets['img/exit.png'];
+	        var GAMEOVER = new Sprite(50, 25);
+		GAMEOVER.image = game.assets['img/gameover.png'];
+	        var PIPE = new Sprite(50, 25);
+		PIPE.image = game.assets['img/kuma_pipe_400_750.png'];
+
+        game.rootScene.addChild(SOSEKI);
         game.addEventListener('enterframe', function() {
-			if(sprite.y<1 && sprite.x<350){
-            sprite.x += 3;
-		}else if(sprite.y<=620 && sprite.x>350)	{
-			sprite.y += 3;
-		}else if(sprite.y>620 && sprite.x >0) {
-			sprite.x -= 3;
-		}else if(sprite.y>0 && sprite.x == 0) {
-			sprite.y -= 3;
+			if(SOSEKI.y<1 && SOSEKI.x<350){
+            SOSEKI.x += 3;
+
+		}else if(SOSEKI.y<=620 && SOSEKI.x>350)	{
+			SOSEKI.y += 3;
+		}else if(SOSEKI.y>620 && SOSEKI.x >0) {
+			SOSEKI.x -= 3;
+		}else if(SOSEKI.y>0 && SOSEKI.x == 0) {
+			SOSEKI.y -= 3;
 		}
     	});
     }
